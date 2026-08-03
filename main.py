8d02f17f80bca062ec8012006da85c1e130fc8e9
Nguyen Tien Manh <manilife217@gmail.com>
2026-08-03 16:09:21 +0700

    test thử với dữ liệu lấy trực tiếp ở trang Báo mới

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from models.use_models.load_models import get_summary
 
 if __name__ == "__main__":
-    get_summary("vit5", "input.txt")
-    get_summary("mt5", "input.txt")
-    get_summary("qwen", "input.txt")
-    get_summary("t5vi", "input.txt")
+    for model_name in ["vit5", "mt5", "qwen", "t5vi"]:
+        for i in range(1, 3):
+            get_summary(model_name, f"input{i}.txt")
