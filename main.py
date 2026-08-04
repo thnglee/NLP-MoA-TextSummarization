11b3bae5c8bbfadbefbaea3a93b5b23f2d9c990f
Nguyen Tien Manh <manilife217@gmail.com>
2026-08-04 10:35:41 +0700

    Tóm tắt thêm 16 bản từ vietnews

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-# from models.use_models.summary_vietnews import run_vit5_on_vietnews_test
-from pathlib import Path
+from models.use_models.summary_vietnews import run_vit5_on_vietnews_test
+# from pathlib import Path
 
-from utils.lexrank import run_lexrank_on_vietnews
-from utils.textrank import run_textrank_on_vietnews
+# from utils.lexrank import run_lexrank_on_vietnews
+# from utils.textrank import run_textrank_on_vietnews
 
 if __name__ == "__main__":
-    # run_vit5_on_vietnews_test(
-    #     dataset_path="dataset/vietnews/test.jsonl",
-    #     output_dir="output/summary/vit5/vietnews_test", start_index=200, max_samples=300,)
+    run_vit5_on_vietnews_test(
+        dataset_path="dataset/vietnews/test.jsonl",
+        output_dir="output/summary/vit5/vietnews_test", start_index=200, max_samples=300,)
 
-    run_textrank_on_vietnews(
-            dataset_path=Path("dataset/vietnews/test.jsonl"),
-            output_dir=Path("output/summary/textrank/vietnews_test"), start_index=1000, max_samples=2000,)
+    # run_textrank_on_vietnews(
+    #         dataset_path=Path("dataset/vietnews/test.jsonl"),
+    #         output_dir=Path("output/summary/textrank/vietnews_test"), start_index=1000, max_samples=2000,)
 
-    run_lexrank_on_vietnews(
-                dataset_path=Path("dataset/vietnews/test.jsonl"),
-                output_dir=Path("output/summary/lexrank/vietnews_test"), start_index=1000, max_samples=2000,)
+    # run_lexrank_on_vietnews(
+    #             dataset_path=Path("dataset/vietnews/test.jsonl"),
+    #             output_dir=Path("output/summary/lexrank/vietnews_test"), start_index=1000, max_samples=2000,)
